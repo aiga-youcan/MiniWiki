d39518a497ef5ceb8714d78e3fc13144a268fc02
aiga-youcan <153380317+aiga-youcan@users.noreply.github.com>
2026-03-25 16:15:52 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const clients = [...]; // Notre Data
-
-// 1. On isole le Callback
-const analyserVIP = (client) => {
-  if (client.totalAchats > 1000) console.log("VIP: " + client.nom);
-};
-
-// 2. On exécute proprement
-clients.forEach(analyserVIP);
